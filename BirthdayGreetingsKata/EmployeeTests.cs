0bfa2c58c025c906effa77e968d18d0d1610f877
Giuseppe Panzuto <giuseppe.panzuto@ynap.com>
2019-09-17 17:18:17 +0200

    EmployeeTests

---
+++ BirthdayGreetingsKata/EmployeeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SystemLevelTtd.BirthdayGreetingsKata
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void GoodDay()
+        {
+            var employee = new Employee("", "", new DateTime(1980, 2, 14), "");
+
+            Assert.True(employee.IsBirthday(new DateTime(2019, 2, 14)));
+        }
+
+        [Fact]
+        public void BadDay()
+        {
+            var employee = new Employee("", "", new DateTime(1980, 2, 14), "");
+
+            Assert.False(employee.IsBirthday(new DateTime(2019, 5, 10)));
+        }
+
+    }
+}
