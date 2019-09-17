89bae9704b59c90eadab6683ad8b2aec6a1a17b6
Giuseppe Panzuto <giuseppe.panzuto@ynap.com>
2019-09-17 12:19:41 +0200

    configurable smtpserver

--- BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
+++ BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
@@ -6,23 +6,26 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
 {
     public class BirthdayGreetingsKataTests : IDisposable
     {
+        private const int SmtpPort = 1030;
+        private const int ApiPort = 1031;
+        private const string SmtpHost = "localhost";
         readonly LocalSmtpServer _smtpServer;
 
         public BirthdayGreetingsKataTests()
         {
-            _smtpServer = new LocalSmtpServer();
-            _smtpServer.StartSmtpServer();
+            _smtpServer = new LocalSmtpServer(SmtpHost, SmtpPort, ApiPort);
+            _smtpServer.Start();
         }
 
         public void Dispose()
         {
-            _smtpServer.StopSmtpServer();
+            _smtpServer.Stop();
         }
 
         [Fact]
         public void SendMail()
         {
-            using (var smtpClient = new SmtpClient("localhost", 1025))
+            using (var smtpClient = new SmtpClient(SmtpHost, SmtpPort))
             {
                 smtpClient.Send("from@a.com", "to@a.com", "SendMail", "body");
             }
--- BirthdayGreetingsKata/LocalSmtpServer.cs
+++ BirthdayGreetingsKata/LocalSmtpServer.cs
@@ -7,16 +7,26 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
     internal class LocalSmtpServer
     {
         private Process smtpServerProcess;
+        private string smtpHost;
+        private int smtpPort;
+        private int apiPort;
 
-        public void StartSmtpServer()
+        public LocalSmtpServer(string smtpHost, int smtpPort, int apiPort)
+        {
+            this.smtpHost = smtpHost;
+            this.smtpPort = smtpPort;
+            this.apiPort = apiPort;
+        }
+
+        public void Start()
         {
             var baseDir = AppDomain.CurrentDomain.BaseDirectory;
             var smtpExePath = Path.Combine(baseDir, "support", "mailhog.exe");
 
-            smtpServerProcess = Process.Start(smtpExePath);
+            smtpServerProcess = Process.Start(smtpExePath, $"-hostname {smtpHost} -smtp-bind-addr :{smtpPort} -api-bind-addr :{apiPort} -ui-bind-addr :{apiPort}");
         }
 
-        public void StopSmtpServer()
+        public void Stop()
         {
             smtpServerProcess.Kill();
             smtpServerProcess.Dispose();
