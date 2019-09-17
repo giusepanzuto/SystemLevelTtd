d5ae2364412125381d8e14fdfec24b2aef1848e3
Giuseppe Panzuto <giuseppe.panzuto@ynap.com>
2019-09-17 12:59:12 +0200

    OneBithday

--- BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
+++ BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SystemLevelTtd.BirthdayGreetingsKata
@@ -9,6 +11,7 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
         private const int SmtpPort = 1030;
         private const int ApiPort = 1031;
         private const string SmtpHost = "localhost";
+        private const string NL = "\r\n";
         readonly LocalSmtpServer _smtpServer;
 
         public BirthdayGreetingsKataTests()
@@ -23,18 +26,63 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
         }
 
         [Fact]
-        public void SendMail()
+        public async Task SendMail()
         {
-            using (var smtpClient = new SmtpClient(SmtpHost, SmtpPort))
-            {
+            using (var smtpClient = new SmtpClient(SmtpHost, SmtpPort)) 
                 smtpClient.Send("from@a.com", "to@a.com", "SendMail", "body");
-            }
+
+            var receivedEmails = await _smtpServer.GetServerInfo();
+
+            Assert.Equal(1, receivedEmails.MailReceived);
         }
 
         [Fact]
-        public void OneBithday()
+        public async Task OneBithday()
         {
-            
+            File.WriteAllLines("test-data.csv", new []
+            {
+                "last_name, first_name, date_of_birth, email",
+                "Capone, Al, 1951-10-08, al.capone@acme.com",
+                "Escobar, Pablo, 1975-09-11, pablo.escobar@acme.com",
+                "Wick, John, 1987-09-11, john.wick@acme.com"
+            });
+
+            var service = new BirthdayGreetingsService(SmtpHost, SmtpPort, "greetings@acme.com");
+            service.SendGreetings(new DateTime(2019, 9, 11));
+
+            var serverInfo = await _smtpServer.GetServerInfo();
+
+            Assert.Equal(1, serverInfo.MailReceived);
+
+            var msg = serverInfo.Messages[0];
+            Assert.Equal("greetings@acme.com", msg.From);
+            Assert.Equal("pablo.escobar@acme.com", msg.To);
+            Assert.Equal("Happy Birthday!", msg.Subject);
+            Assert.Equal("Happy Birthday, dear Pablo!"+ NL, msg.Body);
+        }
+    }
+
+    public class BirthdayGreetingsService
+    {
+        private readonly string from;
+        private readonly string smtpHost;
+        private readonly int smtpPort;
+
+        public BirthdayGreetingsService(string smtpHost, int smtpPort, string @from)
+        {
+            this.smtpHost = smtpHost;
+            this.smtpPort = smtpPort;
+            this.from = from;
+        }
+
+        public void SendGreetings(DateTime today)
+        {
+            var to = "pablo.escobar@acme.com";
+            var subject = "Happy Birthday!";
+            var body = "Happy Birthday, dear Pablo!";
+
+            using (var smtpClient = new SmtpClient(smtpHost, smtpPort))
+                smtpClient.Send(from, to, subject, body);
         }
     }
 }
--- BirthdayGreetingsKata/LocalSmtpServer.cs
+++ BirthdayGreetingsKata/LocalSmtpServer.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SystemLevelTtd.BirthdayGreetingsKata
 {
@@ -30,6 +34,45 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
         {
             smtpServerProcess.Kill();
             smtpServerProcess.Dispose();
+        }
+
+        public async Task<ServerInfo> GetServerInfo()
+        {
+            string httpAddress = $"http://{smtpHost}:{apiPort}/api/v2/messages";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(httpAddress);
+
+            dynamic mails = JObject.Parse(response);
+            int count = mails.count;
+            var messages = new List<MailInfo>();
+            for (int i = 0; i < count; i++)
+            {
+                dynamic msg = mails.items[i];
+                string from = msg.From.Mailbox + "@" + msg.From.Domain;
+                string to = msg.To[0].Mailbox + "@" + msg.To[0].Domain;
+                string subject = msg.Content.Headers.Subject[0];
+                string body = msg.Content.Body;
+                messages.Add(new MailInfo { From = @from, To = to, Subject = subject, Body = body });
+            }
+
+            return new ServerInfo
+            {
+                MailReceived = count,
+                Messages = new List<MailInfo>(messages).ToArray()
+            };
+        }
+        public class MailInfo
+        {
+            public string From { get; set; }
+            public string To { get; set; }
+            public string Subject { get; set; }
+            public string Body { get; set; }
+        }
+
+        public class ServerInfo
+        {
+            public int MailReceived { get; set; }
+            public MailInfo[] Messages { get; set; }
         }
     }
 }
