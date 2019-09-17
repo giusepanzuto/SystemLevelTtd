a2330afd7de5a8000028ea43a30b9e917041e7cf
Giuseppe Panzuto <giuseppe.panzuto@ynap.com>
2019-09-17 11:15:53 +0200

    SendMail tests

--- BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
+++ BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Net.Mail;
 using Xunit;
 
@@ -6,18 +9,20 @@ namespace SystemLevelTtd.BirthdayGreetingsKata
     public class BirthdayGreetingsKataTests
     {
         [Fact]
-        public void ItWorks()
-        {
-            Assert.True(true);
-        }
-
-        [Fact]
         public void SendMail()
         {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var smtpExePath = Path.Combine(baseDir, "support", "mailhog.exe");
+
+            var proc = Process.Start(smtpExePath);
+
             using (var smtpClient = new SmtpClient("localhost", 1025))
             {
                 smtpClient.Send("from@a.com", "to@a.com", "SendMail", "body");
             }
+
+            proc.Kill();
+            proc.Dispose();
         }
 
         [Fact]
