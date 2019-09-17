2ffc41b00b460af65241f067924b916cd429dc03
Giuseppe Panzuto <giuseppe.panzuto@ynap.com>
2019-09-17 10:17:01 +0200

    Add project files.

---
+++ BirthdayGreetingsKata/BirthdayGreetingsKataTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace SystemLevelTtd.BirthdayGreetingsKata
+{
+    public class BirthdayGreetingsKataTests
+    {
+        [Fact]
+        public void ItWorks()
+        {
+            Assert.True(true);
+        }
+    }
+}
